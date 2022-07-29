e831e361b5c39a703a218cd520259959bfd9ec57
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2022-07-29 16:00:34 +0530

    Added api for adding user and finding user by id

--- src/main/java/com/ecom/SpringBoot/controller/UserController.java
+++ src/main/java/com/ecom/SpringBoot/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.ecom.SpringBoot.controller;
 
+import com.ecom.SpringBoot.exceptions.ResourceNotFoundException;
 import com.ecom.SpringBoot.model.User;
 import com.ecom.SpringBoot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,16 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("User doesn't exit with the id: "+id));
+        return ResponseEntity.ok(user);
     }
 }
