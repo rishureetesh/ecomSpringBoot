c7327de1014fd102e6d1cee5de980eac7a386889
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2022-07-29 16:11:25 +0530

    Changes for updaing and deleting user

--- src/main/java/com/ecom/SpringBoot/controller/UserController.java
+++ src/main/java/com/ecom/SpringBoot/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ecom.SpringBoot.exceptions.ResourceNotFoundException;
 import com.ecom.SpringBoot.model.User;
 import com.ecom.SpringBoot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +31,25 @@ public class UserController {
     public ResponseEntity<User> findUserById(@PathVariable Long id){
         User user = userRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("User doesn't exit with the id: "+id));
         return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails){
+        User updateUser = userRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("User doesn't exit with the id: "+id));
+
+        updateUser.setPassword(userDetails.getPassword());
+        updateUser.setFirstname(userDetails.getFirstname());
+        updateUser.setLastname(userDetails.getLastname());
+
+        userRepository.save(updateUser);
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<User> deleteUserById(@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("User doesn't exit with the id: "+id));
+
+        userRepository.delete(user);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
