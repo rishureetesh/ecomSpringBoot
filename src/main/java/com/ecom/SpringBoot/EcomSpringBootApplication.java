47a07602e6dc9e922e7be4caa98117e273a5f026
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2022-07-31 22:49:12 +0530

    Changes for adding swagger for API documentation

--- src/main/java/com/ecom/SpringBoot/EcomSpringBootApplication.java
+++ src/main/java/com/ecom/SpringBoot/EcomSpringBootApplication.java
@@ -1,13 +1,14 @@
 package com.ecom.SpringBoot;
 
-import com.ecom.SpringBoot.model.User;
-import com.ecom.SpringBoot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@OpenAPIDefinition
+@EnableWebMvc
 public class EcomSpringBootApplication {
 
 	public static void main(String[] args) {
