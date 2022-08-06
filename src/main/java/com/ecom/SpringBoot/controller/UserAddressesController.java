c6b88ac2170a49b541aa2da5aab0e6354b1cc4b1
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2022-08-06 09:51:20 +0530

    Changes for adding user cart and fetching cart products

--- src/main/java/com/ecom/SpringBoot/controller/UserAddressesController.java
+++ src/main/java/com/ecom/SpringBoot/controller/UserAddressesController.java
@@ -4,14 +4,12 @@ import com.ecom.SpringBoot.model.UserAddresses;
 import com.ecom.SpringBoot.repository.UserAddressRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/user/addresses")
 public class UserAddressesController {
 
     @Autowired
